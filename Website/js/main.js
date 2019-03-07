8903aa6b731332b1ef640fcacda66a97d5f8216e
Tommy <tommypurkissdev@gmail.com>
2019-03-07 21:55:33 +0000

    commit

--- Website/js/main.js
+++ Website/js/main.js
@@ -17,28 +17,8 @@ function timeGreeting() {
 		divWelcome1.style.background = "url('images/Morning@2x.png')";
 	} else {
 		greeting = 'Hey.';
-		divWelcome1.style.background = "url('images/Hey.png')";
+		divWelcome1.style.background = "url('images/Hey@2x.png')";
 	}
 	document.write(greeting);
 }
 
-//JQuery - Function to enable smooth scrolling across browsers
-$(document).ready(function() {
-	$('a').on('click', function(event) {
-		if (this.hash !== '') {
-			event.preventDefault();
-
-			var hash = this.hash;
-
-			$('html, body').animate(
-				{
-					scrollTop: $(hash).offset().top
-				},
-				300,
-				function() {
-					window.location.hash = hash;
-				}
-			);
-		}
-	});
-});
